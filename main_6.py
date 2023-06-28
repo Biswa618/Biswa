ddf51e5ced757d529ae329cdd7a2e822c317dddf
Biswasubi <biswajith2006@gmail.com>
2023-06-28 10:04:29 +0530

    28-06-2023

--- main_6.py
+++ main_6.py
@@ -1,3 +1,8 @@
-user= input("enter any value: ")
-a = capitalize(user)
-print(a)
+angle1 = int(input("enter an angle: "))
+angle2 = int(input("enter an angle: "))
+angle3 = int(input("enter an angle: "))
+if(angle1 + angle2 + angle3 == 180):
+    print("it is a triangle")
+else:
+    print("it is not a triangle")
+    
---
+++ task.py/main_7.py
@@ -0,0 +1,6 @@
+a=input("enter a value") 
+b=input("enter a value") 
+if(a==b):
+  print("it is equal")
+else:
+   print("it is not equal")
