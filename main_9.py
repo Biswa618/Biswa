5e73eddcf968f0dd9582f86190035127ca73f451
Biswa <136674506+Biswasubi@users.noreply.github.com>
2023-06-28 09:53:43 +0530

    Add files via upload

---
+++ main_9.py
@@ -0,0 +1,18 @@
+a= int(input("enter the units consumed: "))
+b= a-100
+c= b-100
+if(a<=100):
+    print("free free free")
+elif(b<=100 and b>0):
+    d= b*5
+if(c>0):
+    d= (500)+(c*10)
+surcharge= (d*18)/100
+total= d+surcharge
+print("total= ", total)
+
+
+    
+
+    
+
